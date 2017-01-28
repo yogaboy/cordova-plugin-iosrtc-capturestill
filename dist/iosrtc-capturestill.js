715e9c543914cec2e8f99878dc14ce2dc6364c32
yogaboy <mikeblock@rogers.com>
2017-01-28 18:17:35 -0500

    remove commented code

--- dist/iosrtc-capturestill.js
+++ dist/iosrtc-capturestill.js
@@ -1,29 +1,5 @@
 /*global cordova, module*/
 module.exports = MediaStreamRenderer;
-module.exports = {
-	capturestill: function (elementid, successCallback, errorCallback) {
-		if (typeof mediaStreamRenderers === 'undefined') {
-			if (typeof errorCallback == 'function') {
-				errorCallback("mediaStreamRenderers is not defined. Are you loading the iosrtc framework?");
-			} else {
-				throw new Error("mediaStreamRenderers is not defined. Are you loading the iosrtc framework?");
-			}
-			return false;
-		}
-		if (typeof mediaStreamRenderers[elementid] == 'undefined') {
-			if (typeof errorCallback == 'function') {
-				errorCallback("elementid "+elementid+" is not defined in mediaStreamRenderers");
-			} else {
-				throw new Error("elementid "+elementid+" is not defined in mediaStreamRenderers");
-			}
-			return false;
-		}
-//		var keys = Object.keys(mediaStreamRenderers);
-		elementid = mediaStreamRenderers[elementid].id;
-		cordova.exec(successCallback, errorCallback, "iosrtcPlugin", "getStillURI", [elementid]);
-	}
-};
-*/
 
 MediaStreamRenderer.prototype.captureStillAsURI = function(successCB,errorCB) {
 	var self = this;
