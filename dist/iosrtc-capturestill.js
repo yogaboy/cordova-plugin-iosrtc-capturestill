00ed5c41be6a4618a84c3131f43b2a35a5f150a1
yogaboy <mikeblock@rogers.com>
2017-01-28 18:16:05 -0500

    add proper methods

--- dist/iosrtc-capturestill.js
+++ dist/iosrtc-capturestill.js
@@ -1,5 +1,5 @@
 /*global cordova, module*/
-
+module.exports = MediaStreamRenderer;
 module.exports = {
 	capturestill: function (elementid, successCallback, errorCallback) {
 		if (typeof mediaStreamRenderers === 'undefined') {
@@ -20,6 +20,32 @@ module.exports = {
 		}
 //		var keys = Object.keys(mediaStreamRenderers);
 		elementid = mediaStreamRenderers[elementid].id;
-		cordova.exec(successCallback, errorCallback, "iosrtc", "getStillURI", [elementid]);
+		cordova.exec(successCallback, errorCallback, "iosrtcPlugin", "getStillURI", [elementid]);
 	}
 };
+*/
+
+MediaStreamRenderer.prototype.captureStillAsURI = function(successCB,errorCB) {
+	var self = this;
+	exec(successCB,errorCB,"iosrtcPlugin","captureStillAsURI",[self.id]);
+}
+
+function captureStillAsUID(elementid, successCB, errorCB) {
+	if (typeof mediaStreamRenderers === 'undefined') {
+		if (typeof errorCB == 'function') {
+			errorCB("mediaStreamRenderers is not defined. Are you loading the iosrtc framework?");
+		} else {
+			throw new Error("mediaStreamRenderers is not defined. Are you loading the iosrtc framework?");
+		}
+	}
+	if (typeof mediaStreamRenderers[elementid] == 'undefined') {
+		if (typeof errorCB == 'function') {
+			errorCB("elementid "+elementid+" is not defined in mediaStreamRenderers");
+		} else {
+			throw new Error("elementid "+elementid+" is not defined in mediaStreamRenderers");
+		}
+	}
+//		var keys = Object.keys(mediaStreamRenderers);
+	var id = mediaStreamRenderers[elementid].id;
+	cordova.exec(successCB, errorCB, "iosrtcPlugin", "captureStillAsURI", [id]);
+}
